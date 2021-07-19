8ad1ed7a488236ea6f00a651f83a69a5a5ef61d6
MaxJGruber <maxjgruber@gmail.com>
2021-07-19 11:59:16 +0200

    changes to server.js

--- src/ApiHandlers/wufooApi.ts
+++ src/ApiHandlers/wufooApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: `http://localhost:${process.env.BACKEND_PORT}`,
   withCredentials: true,
 });
 
