dcf4cb272f12139816f68b6fe7db524e26d0a8c1
Max <maxjgruber@gmail.com>
2022-12-16 15:43:59 +0100

    Update English project text

--- content/English/Projects/projectsList.ts
+++ content/English/Projects/projectsList.ts
@@ -16,18 +16,69 @@ export const projectsList = {
   ],
   projects: [
     {
-      name: "Europe Sustainable Development Report 2021",
+      name: "Europe Sustainable Development Report 2022",
       type: "Data Visualization",
-      intro: "Second project at SDSN",
-      technologies: "Next.Js",
+      intro: "Fifth project at SDSN",
+      technologies: "Next.Js / Nivo charts",
       description:
-        "The Europe Sustainable Development Report 2021 is the third edition of SDSN's independent quantitative report on the progress of the European Union and its member states towards Sustainable Development Goals (SDGs).",
-      specialFeatures: "",
+        "The Europe Sustainable Development Report 2022 (4th edition) was prepared by a team of independent researchers at the UN Sustainable Development Solutions Network (SDSN) in collaboration with SDSN Europe. It builds on the methodology of the annual Sustainable Development Report, including the SDG Index and Dashboards, issued by the SDSN and Bertelsmann Stiftung since 2016.",
+      specialFeatures:
+        "The data explorer: A tool customize your visual charts and deep-dive into the data",
       landingPagePic: "static/project-pics/esdr2021.gif",
       contain: false,
       projectLink: "https://eu-dashboards.sdgindex.org/",
       stats: [
-        { label: "Created", value: "2021" },
+        { label: "Created", value: "2022" },
+        { label: "Duration", value: "3 weeks" },
+      ],
+    },
+    {
+      name: "Charles Dumeige Osteomeaux",
+      type: "Advanced Brochure Website",
+      intro: "Update of my first freelance mission",
+      technologies: "Next.Js / Tailwind",
+      description:
+        "Freelance mission project for the same osteopath but this time for Sartrouville and Paris, France. The website works as an electronic business card for the client and it also has the function to explain in greater detail what his profession consists of.",
+      specialFeatures: "",
+      landingPagePic: "static/project-pics/osteomeaux-demo.gif",
+      contain: true,
+      projectLink: "https://www.charles-dumeige-osteopathe.fr/",
+      stats: [
+        { label: "Updated", value: "2022" },
+        { label: "Duration", value: "2 weeks" },
+      ],
+    },
+    {
+      name: "Benin Sustainable Development Report 2022",
+      type: "Data Visualization",
+      intro: "Fourth project at SDSN",
+      technologies: "Next.Js",
+      description:
+        "It presents Benin's starting point in its new commitment to the implementation of the 2030 Agenda following the inaugural issue of the SDG Eurobond in 2021. It is the first report to provide an independent analysis of the country's SDG performance and trends compared to the Economic Community of West African States (ECOWAS) as well as the policies and progress.",
+      specialFeatures: "",
+      landingPagePic: "static/project-pics/esdr2021.gif",
+      contain: false,
+      projectLink:
+        "https://www.sdgindex.org/reports/benin-sustainable-development-report-2022/",
+      stats: [
+        { label: "Created", value: "2022" },
+        { label: "Duration", value: "3 weeks" },
+      ],
+    },
+    {
+      name: "Sustainable Development Report 2022",
+      type: "Data Visualization",
+      intro: "Third project at SDSN",
+      technologies: "Next.Js / Nivo charts",
+      description:
+        "Peace, diplomacy, and international cooperation are fundamental conditions for the world to progress on the SDGs towards 2030 and beyond. For the second year in a row, the world is no longer making progress on the SDGs. A global plan to finance the SDGs is needed. At mid-point on the way to 2030, policy efforts and commitments supporting the SDGs vary significantly across countries, including among G20 countries.",
+      specialFeatures:
+        "The data explorer: A tool customize your visual charts and deep-dive into the data",
+      landingPagePic: "static/project-pics/esdr2021.gif",
+      contain: false,
+      projectLink: "https://dashboards.sdgindex.org/",
+      stats: [
+        { label: "Created", value: "2022" },
         { label: "Duration", value: "6 weeks" },
       ],
     },
@@ -43,11 +94,7 @@ export const projectsList = {
       landingPagePic: "static/project-pics/impact-investment-toolkit.gif",
       contain: false,
       projectLink: "https://sdg-investment-tool-beta.netlify.app/",
-      stats: [
-        { label: "Expected", value: "TBD" },
-        // { label: "Created", value: "2021" },
-        // { label: "Duration", value: "3 weeks" },
-      ],
+      stats: [{ label: "Expected", value: "TBD" }],
     },
     {
       name: "Marss N30",
@@ -59,7 +106,6 @@ export const projectsList = {
       specialFeatures: "",
       landingPagePic: "static/project-pics/marss-n30-demo.gif",
       contain: false,
-      projectLink: "https://www.marss-n30.com/",
       stats: [
         { label: "Created", value: "2021" },
         { label: "Duration", value: "3 weeks" },
@@ -75,7 +121,7 @@ export const projectsList = {
       specialFeatures: "",
       landingPagePic: "static/project-pics/osteomeaux-demo.gif",
       contain: true,
-      projectLink: "https://www.charles-dumeige-osteomeaux.fr/",
+      projectLink: "https://github.com/MaxJGruber/Osteopathe_cd_Portfolio",
       stats: [
         { label: "Created", value: "2021" },
         { label: "Duration", value: "8 weeks" },
@@ -93,7 +139,7 @@ export const projectsList = {
         "Using MapBox, full user authentication using React.JS, use of libraries such as Semantics UI and Material UI.",
       landingPagePic: "static/project-pics/cosmousse-demo.gif",
       contain: true,
-      projectLink: "https://cosmousse.herokuapp.com/",
+      projectLink: "https://github.com/search?q=cosmousse&type=repositories",
       stats: [
         { label: "Created", value: "2020" },
         { label: "Duration", value: "10 days" },
@@ -110,7 +156,6 @@ export const projectsList = {
       specialFeatures: "",
       landingPagePic: "static/project-pics/ekam-demo.gif",
       contain: false,
-      projectLink: "https://ekam-games.herokuapp.com/",
       stats: [
         { label: "Created", value: "2020" },
         { label: "Duration", value: "5 days" },
@@ -125,7 +170,7 @@ export const projectsList = {
         "An imitation of world-known video game Guitar Hero where the player needs to hit the notes as they align with the fret-board. The higher your streak, the higher the points multiply.",
       specialFeatures: "",
       landingPagePic: "static/project-pics/iron-guitar-demo.gif",
-      projectLink: "https://maxjgruber.github.io/Iron-Guitar/",
+      projectLink: "https://github.com/MaxJGruber/Iron-Guitar",
       contain: false,
       stats: [
         { label: "Created", value: "2020" },
