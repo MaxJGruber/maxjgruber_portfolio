9cc94d45dd0e20d6ee85b05adc262e5b35ae7484
Max <maxjgruber@gmail.com>
2024-01-05 13:34:04 +0100

    Implement Montserrat font correctly

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,12 @@ const Document = () => (
       <meta charSet="utf-8" />
       <link rel="icon" href="static/favicon.ico" />
       <meta name="theme-color" content="#000000" />
-      <script
-        type="text/javascript"
-        async
-        src="https://tenor.com/embed.js"
-      ></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
+        rel="stylesheet"
+      />
       {/* Global Site Tag - Google Analytics */}
       <Script
         async
@@ -35,7 +36,7 @@ const Document = () => (
       />
       {/* End of Global Site Tag - Google Analytics */}
     </Head>
-    <body>
+    <body className="font-montserrat">
       <Main />
       <NextScript />
     </body>
