f195afe6ad0c63b6edea01e2c078a7dde697c905
Max <maxjgruber@gmail.com>
2024-01-05 15:53:25 +0100

    Remove deprecated store

--- stores/languageContent.ts
+++
@@ -1,9 +0,0 @@
-import { content } from "content/index";
-import store from "stores/store";
-
-function contentLanguage() {
-  const language: "en" | "fr" | "de" = store.getState().language.language;
-  return content[language];
-}
-
-export default contentLanguage;
