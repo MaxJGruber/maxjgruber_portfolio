adf802d02f1dce3a7978029185366a2e0c3418a8
Max <maxjgruber@gmail.com>
2024-01-05 13:46:46 +0100

    Revert "Separate index into its layout and its data"
    This reverts commit d6e5137418bb3d4eadf9622afcc53427662d7f9c.

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import { Router } from "next/router";
+import { Provider } from "react-redux";
+import { enableStaticRendering } from "mobx-react-lite";
 import { DefaultSeo } from "next-seo";
 import { TITLE, META_DESCRIPTION, META_IMAGE, URL } from "root/config";
 import * as gtag from "helpers/gtag";
+import store from "stores/store";
+import changeLanguage from "stores/languageContent";
 import "styles/globals.css";
 
-import AppLayout from "@/layouts/AppLayout";
+const App = ({ Component, pageProps }) => {
+  if (typeof window === undefined) {
+    enableStaticRendering(true);
+  }
+  // Remove the server-side injected CSS.
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-const App = ({ Component: NextPage, pageProps }) => {
+  // eslint-disable-next-line
+  const [content, setContent] = useState(null);
+  useEffect(() => {
+    store.subscribe(() => {
+      store.getState();
+      setContent(changeLanguage());
+    }, []);
+  });
+
   // Track pages with google analytics
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -21,30 +43,23 @@ const App = ({ Component: NextPage, pageProps }) => {
   }, []);
   // End of track pages with google analytics
 
-  const {
-    Layout = ({ children, language }) => (
-      <AppLayout language={language}>{children}</AppLayout>
-    ),
-    layoutProps = () => {},
-  } = NextPage;
-
   return (
     <div className="App">
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+      <Provider store={store}>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <DefaultSeo
+          title={TITLE}
+          description={META_DESCRIPTION}
+          openGraph={{ url: URL, images: [{ url: META_IMAGE }] }}
+          twitter={{ cardType: "summary_large_image" }}
         />
-      </Head>
-      <DefaultSeo
-        title={TITLE}
-        description={META_DESCRIPTION}
-        openGraph={{ url: URL, images: [{ url: META_IMAGE }] }}
-        twitter={{ cardType: "summary_large_image" }}
-      />
-      <Layout {...layoutProps(pageProps)}>
-        <NextPage {...pageProps} />
-      </Layout>
+        <Component {...pageProps} />
+      </Provider>
     </div>
   );
 };
