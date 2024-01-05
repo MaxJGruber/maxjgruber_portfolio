c4cc71134bf3dcac74b6a907d1a89f8f1d4796f9
Max <maxjgruber@gmail.com>
2024-01-05 17:27:09 +0100

    Refactor next config file env vars

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
   output: "export",
   env: {
-    WUFOO_BASE_URL: process.env.NEXT_PUBLIC_WUFOO_BASE_URL,
-    WUFOO_API_KEY: process.env.NEXT_PUBLIC_WUFOO_API_KEY,
-    WUFOO_FORM_ID: process.env.NEXT_PUBLIC_WUFOO_FORM_ID,
-    WUFOO_PASSWORD: process.env.NEXT_PUBLIC_WUFOO_PASSWORD,
+    WUFOO_BASE_URL: "NEXT_PUBLIC_WUFOO_BASE_URL",
+    WUFOO_API_KEY: "NEXT_PUBLIC_WUFOO_API_KEY",
+    WUFOO_FORM_ID: "NEXT_PUBLIC_WUFOO_FORM_ID",
+    WUFOO_PASSWORD: "NEXT_PUBLIC_WUFOO_PASSWORD",
   },
 };
+
+module.exports = nextConfig;
