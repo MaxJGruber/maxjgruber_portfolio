d6e5137418bb3d4eadf9622afcc53427662d7f9c
Max <maxjgruber@gmail.com>
2024-01-05 13:45:18 +0100

    Separate index into its layout and its data

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,14 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Head from "next/head";
 import { Router } from "next/router";
-import { Provider } from "react-redux";
-import { enableStaticRendering } from "mobx-react-lite";
 import { DefaultSeo } from "next-seo";
 import { TITLE, META_DESCRIPTION, META_IMAGE, URL } from "root/config";
 import * as gtag from "helpers/gtag";
-import store from "stores/store";
-import changeLanguage from "stores/languageContent";
 import "styles/globals.css";
 
-const App = ({ Component, pageProps }) => {
-  if (typeof window === undefined) {
-    enableStaticRendering(true);
-  }
-  // Remove the server-side injected CSS.
-  useEffect(() => {
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+import AppLayout from "@/layouts/AppLayout";
 
-  // eslint-disable-next-line
-  const [content, setContent] = useState(null);
-  useEffect(() => {
-    store.subscribe(() => {
-      store.getState();
-      setContent(changeLanguage());
-    }, []);
-  });
-
+const App = ({ Component: NextPage, pageProps }) => {
   // Track pages with google analytics
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -43,23 +21,30 @@ const App = ({ Component, pageProps }) => {
   }, []);
   // End of track pages with google analytics
 
+  const {
+    Layout = ({ children, language }) => (
+      <AppLayout language={language}>{children}</AppLayout>
+    ),
+    layoutProps = () => {},
+  } = NextPage;
+
   return (
     <div className="App">
-      <Provider store={store}>
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
-        <DefaultSeo
-          title={TITLE}
-          description={META_DESCRIPTION}
-          openGraph={{ url: URL, images: [{ url: META_IMAGE }] }}
-          twitter={{ cardType: "summary_large_image" }}
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <Component {...pageProps} />
-      </Provider>
+      </Head>
+      <DefaultSeo
+        title={TITLE}
+        description={META_DESCRIPTION}
+        openGraph={{ url: URL, images: [{ url: META_IMAGE }] }}
+        twitter={{ cardType: "summary_large_image" }}
+      />
+      <Layout {...layoutProps(pageProps)}>
+        <NextPage {...pageProps} />
+      </Layout>
     </div>
   );
 };
