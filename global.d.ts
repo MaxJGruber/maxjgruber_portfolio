42577ee58ebd007b5b06f937359c0fadbf35bfd7
Max <maxjgruber@gmail.com>
2024-01-05 15:51:13 +0100

    Declare icon types in repo

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,7 @@
-declare module "*.pdf";
 declare module "@heroicons/react/*";
+
+declare global {
+  import React from "react";
+  declare const HeroIconType: React.SFC<MyComponentProps>;
+  export default HeroIconType;
+}
