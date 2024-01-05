b04c6f73ade293d753d68e80deab4a8bacd1061e
Max <maxjgruber@gmail.com>
2024-01-05 17:31:39 +0100

    Revert "Refactor next config file env vars"
    This reverts commit c4cc71134bf3dcac74b6a907d1a89f8f1d4796f9.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   output: "export",
   env: {
-    WUFOO_BASE_URL: "NEXT_PUBLIC_WUFOO_BASE_URL",
-    WUFOO_API_KEY: "NEXT_PUBLIC_WUFOO_API_KEY",
-    WUFOO_FORM_ID: "NEXT_PUBLIC_WUFOO_FORM_ID",
-    WUFOO_PASSWORD: "NEXT_PUBLIC_WUFOO_PASSWORD",
+    WUFOO_BASE_URL: process.env.NEXT_PUBLIC_WUFOO_BASE_URL,
+    WUFOO_API_KEY: process.env.NEXT_PUBLIC_WUFOO_API_KEY,
+    WUFOO_FORM_ID: process.env.NEXT_PUBLIC_WUFOO_FORM_ID,
+    WUFOO_PASSWORD: process.env.NEXT_PUBLIC_WUFOO_PASSWORD,
   },
 };
-
-module.exports = nextConfig;
