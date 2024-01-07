88a40062182958919741f217029074ed02e35a6f
Max <maxjgruber@gmail.com>
2024-01-07 15:47:16 +0100

    CYPRESS: Add supportFile: false to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,7 @@ module.exports = defineConfig({
   blockHosts: ["www.google-analytics.com", "www.googletagmanager.com"],
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
+    supportFile: false,
     baseUrl: "http://localhost:3000/",
     defaultCommandTimeout: 5000,
     testIsolation: false,
