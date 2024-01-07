e08b1789e625234b175eb5737c8647f895a63cff
Max <maxjgruber@gmail.com>
2024-01-07 15:44:51 +0100

    CYPRESS: Add import to cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,11 @@ module.exports = defineConfig({
   blockHosts: ["www.google-analytics.com", "www.googletagmanager.com"],
   video: false,
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
     baseUrl: "http://localhost:3000/",
     defaultCommandTimeout: 5000,
     testIsolation: false,
